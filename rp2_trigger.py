0e6332334b59bf0c2593cb8b6a4b98924545c37e
DrSpeed <doctor_speed_2001@yahoo.com>
2016-06-27 13:56:51 -0700

    Gracenote Hackathon work

---
+++ rp2_trigger.py
@@ -0,0 +1,41 @@
+import subprocess
+import shutil
+import time
+import os
+import RPi.GPIO as io
+import sqlite3 as lite
+import time
+
+# set up GPIO
+io.setmode(io.BCM)
+pir_pin = 4
+
+#--files--
+db = '/home/pi/sqlite/camera_db'
+capshell = '~pi/git/RaspberryPi/PostToElastic.sh'
+#---
+
+motionState = False;
+
+io.setup(pir_pin, io.IN)         # activate input
+
+
+while True:
+    if io.input(pir_pin):
+        if motionState == False:
+            print("PIR ALARM!")
+            motionState = True;
+            start = time.time()
+
+    else:
+        if motionState == True:
+            print("PIR OFF!")
+            end = time.time()
+            delta = (end - start)
+            deltaStr = "{:0>0}".format(delta)
+            deltaInt = int(delta)
+            print('Time diff: ')
+            print(deltaInt)
+            os.system('sudo ' + capshell + ' ' + str(deltaInt))
+            motionState = False;
+    time.sleep(1.0)
