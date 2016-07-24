6043852bae051bbd59f778f699e2ea74d2df69e7
SJW <doctor_speed_2001@yahoo.com>
2016-07-24 01:27:12 +0000

    added utility to store a uri file to local filesystem

---
+++ node_code/getfileparams.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+
+var PARAM_0 = 2;
+var PARAM_1 = 3
+
+// 
+var file_url    = process.argv[PARAM_0];
+var output_file = process.argv[PARAM_1];
+
+var file = fs.createWriteStream(output_file);
+var request = http.get(file_url, function(response) {
+    console.log("Writing to file " + output_file);
+    response.pipe(file);
+});
+
+
+
