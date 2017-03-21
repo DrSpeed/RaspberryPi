0a5a379693cc19937e3adb5ef080db416aaebb9c
DrSpeed <doctor_speed_2001@yahoo.com>
2017-03-21 09:16:19 -0700

    Added comment

--- capture.sh
+++ capture.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# called via cron with something like :
+# (ignore hash)*/5  *  * * *   root   /var/www/html/sunrise_set.sh 
+
 DATE_STR=$(date +\%a_\%H_\%M_\%p)
 /usr/bin/raspistill -n -o  /home/pi/node/img/capture_${DATE_STR}.jpg
 /usr/bin/raspistill -n -w 128 -h 128 -o  /home/pi/node/img/capture_${DATE_STR}_th.jpg
